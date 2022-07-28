d5c0651147019f3f4a938bfefe9acbdcb02e1d4a
saisubhasish <102937478+saisubhasish@users.noreply.github.com>
2022-07-29 00:07:15 +0530

    Add files via upload

---
+++ Programming_Assingment1.py
@@ -0,0 +1,53 @@
+# 1.
+"""
+    This is my python program to print "Hello Python"
+"""
+print('Hello Python')
+
+
+# 2.
+"""
+    This is my python program for arithmetical operations addition and division
+"""
+a = 18
+b = 2
+print(a*b)
+print(a/b)
+
+# 3.
+"""
+    This is my python program  to find the area of a triangle
+"""
+b = 5
+h = 10
+print('Area :', 1/2*b*h)
+
+# 4.
+"""
+    This is my python program  to swap two variables with the use of third variable
+"""
+a = 10
+b = 20
+c = 0
+c = a
+a = b
+b = c
+print(a,b)
+
+#or
+"""
+    This is my python program  to swap two variables with our use of third variable
+"""
+a = 10
+b = 20
+a = a+b
+b = a-b
+a = a-b
+print(a,b)
+
+# 5.
+"""
+    This is my python program  to generate a random number
+"""
+import random
+print(random.randint(0,9))
