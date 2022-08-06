d94732309bd6240994a4c1dd69acbb145a9b7f05
saisubhasish <102937478+saisubhasish@users.noreply.github.com>
2022-08-06 18:00:51 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -3,4 +3,4 @@ df1 = pd.read_csv('D:/FSDS-iNeuron/3.Resource/dataFSDS/haberman.csv', header= No
 df2 = pd.read_csv('D:/FSDS-iNeuron/3.Resource/dataFSDS/haberman(1).csv', sep='@')  # online json viewer
 pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/Smarket.csv')
 a = pd.read_html('https://www.basketball-reference.com/leagues/NBA_2015_totals.html')
-df.to_csv('D:/FSDS-iNeuron/3.Resource/dataFSDS/AttributeDataset.csv', sep='#')
+df.to_csv('D:/FSDS-iNeuron/3.Resource/dataFSDS/AttributeDataset.csv', sep='#', index=False)
