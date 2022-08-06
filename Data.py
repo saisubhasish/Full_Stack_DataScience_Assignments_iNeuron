1e58346d7a9542c30afcd2b5a6933d9339c002a4
saisubhasish <102937478+saisubhasish@users.noreply.github.com>
2022-08-06 17:10:01 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,4 +1,1 @@
-d = {
-    'name' : 'Sai',
-    'home' : 'Jajpur'
-}
+df = pd.read_excel('D:/FSDS-iNeuron/3.Resource/dataFSDS/Attribute DataSet.xlsx', sheet_name = 'Sai', header= None, names= ['a', 'b', 'c', 'd'])
